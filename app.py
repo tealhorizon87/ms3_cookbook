48d7a966aa317cb463c7a6e9d97fefc954b78d6a
tealhoion87 <nealtoms@gmail.com>
2021-09-09 15:49:04 +0100

    Add login functionality and a profile page
      - add python code to wire up the login form
      - add profile html template
      - add validation requirements for the input fields of the login
        and register forms
      - add python code to render profile template and include the
        username from session data

--- app.py
+++ app.py
@@ -47,14 +47,51 @@ def register():
         }
         # enter the new user to the users collection
         mongo.db.users.insert_one(register)
+        session["user"] = request.form.get("username").lower()
         flash("Registration Successful")
+        return redirect(url_for("profile", username = session["user"]))
+
 
     return render_template("register.html")
 
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # check if user already exsists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # check password is correct
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome back, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username = session["user"]))
+            else:
+                flash("Incorrect uername and/or password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods = ["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username = username)
+
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
