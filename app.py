943cc083d7615144ea39328df0f7e3e8c399c967
tealhoion87 <nealtoms@gmail.com>
2021-09-24 16:54:40 +0100

    Change favorites and flash messages
      - add the ability to toggle a recipe to and from the user's favourite
        list
      - add a colour coding with categories to all flash messages

--- app.py
+++ app.py
@@ -37,10 +37,10 @@ def register():
         confirm_password = request.form.get("password-confirm")
 
         if existing_user:
-            flash("Username already exists")
+            flash("Username already exists", "error")
             return redirect(url_for("register"))
         elif password != confirm_password:
-            flash("Your password and confirmation do not match")
+            flash("Your password and confirmation do not match", "error")
             return redirect(url_for("register"))
 
         register = {
@@ -51,7 +51,7 @@ def register():
         # enter the new user to the users collection
         mongo.db.users.insert_one(register)
         session["user"] = request.form.get("username").lower()
-        flash("Registration Successful")
+        flash("Registration Successful", "success")
         return redirect(url_for("profile", username = session["user"]))
 
 
@@ -72,16 +72,16 @@ def login():
                     # Add user to session cookies
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome back, {}".format(
-                        request.form.get("username")))
+                        request.form.get("username")), "success")
                     return redirect(url_for(
                         "profile", username = session["user"]))
             else:
-                flash("Incorrect uername and/or password")
+                flash("Incorrect uername and/or password", "error")
                 return redirect(url_for("login"))
 
         else:
             # username doesn't exist
-            flash("Incorrect Username and/or Password")
+            flash("Incorrect Username and/or Password", "error")
             return redirect(url_for("login"))
 
     return render_template("login.html")
@@ -108,7 +108,7 @@ def profile(username):
 @app.route("/logout")
 def logout():
     # remove user from session cookies
-    flash("You have been logged out")
+    flash("You have been logged out", "success")
     session.pop("user")
     return redirect(url_for("home"))
 
@@ -116,7 +116,11 @@ def logout():
 @app.route("/recipe/<recipe_id>", methods = ["GET", "POST"])
 def recipe(recipe_id):
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
-    return render_template("recipe.html", recipe = recipe)
+    favorites = mongo.db.users.find_one(
+        {"username": session["user"]})["my_favorites"]
+    is_favorite = "yes" if recipe_id in favorites else "no"
+    return render_template("recipe.html", recipe = recipe,
+        favorites = favorites, is_favorite = is_favorite)
 
 
 @app.route("/add_recipe", methods = ["GET", "POST"])
@@ -140,7 +144,7 @@ def add_recipe():
             "date_created": datetime.now()
         }
         mongo.db.recipes.insert_one(recipe)
-        flash("Recipe successfully uploaded!")
+        flash("Recipe successfully uploaded!", "success")
         return redirect(url_for("home"))
 
     categories = mongo.db.categories.find().sort("category_name", 1)
@@ -170,7 +174,7 @@ def edit_recipe(recipe_id):
             "date_created": datetime.now()
         }
         mongo.db.recipes.update({"_id": ObjectId(recipe_id)}, submit)
-        flash("Recipe updated")
+        flash("Recipe updated", "success")
         return redirect(url_for("profile", username = session['user']))
 
     categories = mongo.db.categories.find().sort("category_name", 1)
@@ -187,7 +191,7 @@ def edit_recipe(recipe_id):
 @app.route("/delete_recipe/<recipe_id>")
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
-    flash("Recipe Successfully Deleted")
+    flash("Recipe Successfully Deleted", "success")
     return redirect(url_for("profile", username = session['user']))
 
 
@@ -199,7 +203,7 @@ def search():
     if recipes:
         return render_template("home.html", recipes = recipes)
     else:
-        flash("No results found")
+        flash("No results found", "error")
         return redirect(url_for("home"))
 
 
@@ -210,7 +214,7 @@ def edit_category(category_id):
             "category_name": request.form.get("category")
         }
         mongo.db.categories.update({"_id": ObjectId(category_id)}, submit)
-        flash("Category Updated")
+        flash("Category Updated", "success")
         return redirect(url_for("profile", username = session['user']))
 
     category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
@@ -224,14 +228,14 @@ def add_category():
             "category_name": request.form.get("category")
         }
         mongo.db.categories.insert_one(category)
-        flash("Category Added")
+        flash("Category Added", "success")
         return redirect(url_for("profile", username = session['user']))
 
 
 @app.route("/delete_category/<category_id>")
 def delete_category(category_id):
     mongo.db.categories.remove({"_id": ObjectId(category_id)})
-    flash("Category Successfully Deleted")
+    flash("Category Successfully Deleted", "success")
     return redirect(url_for("profile", username = session['user']))
 
 
@@ -243,7 +247,7 @@ def edit_user(user_id):
             "username": request.form.get("username"),
         }
         mongo.db.users.update({"_id": ObjectId(user_id)}, submit)
-        flash("User Details Updated")
+        flash("User Details Updated", "success")
         return redirect(url_for("profile", username = session['user']))
 
     user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
@@ -253,7 +257,7 @@ def edit_user(user_id):
 @app.route("/delete_user/<user_id>")
 def delete_user(user_id):
     mongo.db.users.remove({"_id": ObjectId(user_id)})
-    flash("User Successfully Deleted")
+    flash("User Successfully Deleted", "success")
     return redirect(url_for("profile", username = session['user']))
 
 
@@ -263,9 +267,18 @@ def add_favorite(recipe_id):
         {"username": session["user"]})["_id"]
     mongo.db.users.update({"_id": ObjectId(user_id)}, {"$push": {
         "my_favorites": recipe_id}})
-    flash("Added to favorites")
+    flash("Added to favorites", "success")
     return redirect(url_for("recipe", recipe_id = recipe_id))
 
+
+@app.route("/delete_favorite/<recipe_id>")
+def delete_favorite(recipe_id):
+    user_id = mongo.db.users.find_one(
+        {"username": session["user"]})["_id"]
+    mongo.db.users.update({"_id": ObjectId(user_id)}, {"$pull": {
+        "my_favorites": recipe_id}})
+    flash("Removed from favorites", "success")
+    return redirect(url_for("recipe", recipe_id = recipe_id))
 
 if __name__ == "__main__":
     app.run(host = os.environ.get("IP"),
