ea37314e57a854f56f364f1e0055180723cc2ab8
tealhoion87 <nealtoms@gmail.com>
2021-09-27 16:47:30 +0100

    Fix python code for pep8 compliance

--- app.py
+++ app.py
@@ -23,10 +23,10 @@ mongo = PyMongo(app)
 def home():
     recipes = list(mongo.db.recipes.find(
         {"is_private": "off"}).sort("date_created", -1).limit(20))
-    return render_template("home.html", recipes = recipes)
+    return render_template("home.html", recipes=recipes)
 
 
-@app.route("/register", methods = ["GET", "POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         # check if user already exsists
@@ -53,13 +53,12 @@ def register():
         mongo.db.users.insert_one(register)
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful", "success")
-        return redirect(url_for("profile", username = session["user"]))
-
+        return redirect(url_for("profile", username=session["user"]))
 
     return render_template("register.html")
 
 
-@app.route("/login", methods = ["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         # check if user already exsists
@@ -68,14 +67,14 @@ def login():
 
         if existing_user:
             # check password is correct
-            if check_password_hash(
-                existing_user["password"], request.form.get("password")):
+            if check_password_hash(existing_user["password"],
+                                   request.form.get("password")):
                     # Add user to session cookies
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome back, {}".format(
                         request.form.get("username")), "success")
                     return redirect(url_for(
-                        "profile", username = session["user"]))
+                        "profile", username=session["user"]))
             else:
                 flash("Incorrect uername and/or password", "error")
                 return redirect(url_for("login"))
@@ -88,7 +87,7 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/profile/<username>", methods = ["GET", "POST"])
+@app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
@@ -97,11 +96,13 @@ def profile(username):
         recipes = list(mongo.db.recipes.find({"created_by": session["user"]}))
         favorites = mongo.db.users.find_one(
             {"username": session["user"]})["my_favorites"]
-        my_favorites = [mongo.db.recipes.find_one({"_id": ObjectId(favorite)}) for favorite in favorites]
+        my_favorites = [mongo.db.recipes.find_one(
+            {"_id": ObjectId(favorite)}) for favorite in favorites]
         users = mongo.db.users.find().sort("username", 1)
         categories = mongo.db.categories.find().sort("category_name", 1)
-        return render_template("profile.html", username = username,
-            recipes = recipes, categories = categories, users = users, my_favorites = my_favorites)
+        return render_template("profile.html", username=username,
+                               recipes=recipes, categories=categories,
+                               users=users, my_favorites=my_favorites)
 
     return redirect(url_for("login"))
 
@@ -114,20 +115,20 @@ def logout():
     return redirect(url_for("home"))
 
 
-@app.route("/recipe/<recipe_id>", methods = ["GET", "POST"])
+@app.route("/recipe/<recipe_id>", methods=["GET", "POST"])
 def recipe(recipe_id):
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     if session:
         favorites = mongo.db.users.find_one(
             {"username": session["user"]})["my_favorites"]
         is_favorite = "yes" if recipe_id in favorites else "no"
-        return render_template("recipe.html", recipe = recipe,
-            favorites = favorites, is_favorite = is_favorite)
+        return render_template("recipe.html", recipe=recipe,
+                               favorites=favorites, is_favorite=is_favorite)
 
-    return render_template("recipe.html", recipe = recipe)
+    return render_template("recipe.html", recipe=recipe)
 
 
-@app.route("/add_recipe", methods = ["GET", "POST"])
+@app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
     if request.method == "POST":
         is_private = "on" if request.form.get("is_private") else "off"
@@ -149,15 +150,15 @@ def add_recipe():
         }
         mongo.db.recipes.insert_one(recipe)
         flash("Recipe successfully uploaded!", "success")
-        return redirect(url_for("profile", username = session['user']))
+        return redirect(url_for("profile", username=session['user']))
 
     categories = mongo.db.categories.find().sort("category_name", 1)
     diets = mongo.db.diet.find().sort("diet_name", 1)
     return render_template(
-        "add-recipe.html", categories = categories, diets = diets)
+        "add-recipe.html", categories=categories, diets=diets)
 
 
-@app.route("/edit_recipe/<recipe_id>", methods = ["GET", "POST"])
+@app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
     if request.method == "POST":
         is_private = "on" if request.form.get("is_private") else "off"
@@ -179,7 +180,7 @@ def edit_recipe(recipe_id):
         }
         mongo.db.recipes.update({"_id": ObjectId(recipe_id)}, submit)
         flash("Recipe updated", "success")
-        return redirect(url_for("profile", username = session['user']))
+        return redirect(url_for("profile", username=session['user']))
 
     categories = mongo.db.categories.find().sort("category_name", 1)
     diets = mongo.db.diet.find().sort("diet_name", 1)
@@ -188,30 +189,31 @@ def edit_recipe(recipe_id):
     ingredients = '*'.join(recipe["ingredients"])
     steps = '*'.join(recipe["steps"])
     return render_template("edit-recipe.html",
-        recipe = recipe, diets = diets, categories = categories,
-        allergens = allergens, ingredients = ingredients, steps = steps)
+                           recipe=recipe, diets=diets, categories=categories,
+                           allergens=allergens, ingredients=ingredients,
+                           steps=steps)
 
 
 @app.route("/delete_recipe/<recipe_id>")
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
     flash("Recipe Successfully Deleted", "success")
-    return redirect(url_for("profile", username = session['user']))
+    return redirect(url_for("profile", username=session['user']))
 
 
-@app.route("/search", methods = ["GET", "POST"])
+@app.route("/search", methods=["GET", "POST"])
 def search():
     query = request.form.get("query")
     recipes = list(mongo.db.recipes.find(
         {"is_private": "off", "$text": {"$search": query}}))
     if recipes:
-        return render_template("home.html", recipes = recipes)
+        return render_template("home.html", recipes=recipes)
     else:
         flash("No results found", "error")
         return redirect(url_for("home"))
 
 
-@app.route("/edit_category/<category_id>", methods = ["GET", "POST"])
+@app.route("/edit_category/<category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
     if request.method == "POST":
         submit = {
@@ -219,13 +221,13 @@ def edit_category(category_id):
         }
         mongo.db.categories.update({"_id": ObjectId(category_id)}, submit)
         flash("Category Updated", "success")
-        return redirect(url_for("profile", username = session['user']))
+        return redirect(url_for("profile", username=session['user']))
 
     category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
-    return render_template("edit-category.html", category = category)
+    return render_template("edit-category.html", category=category)
 
 
-@app.route("/add_category", methods = ["GET", "POST"])
+@app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
         category = {
@@ -233,17 +235,17 @@ def add_category():
         }
         mongo.db.categories.insert_one(category)
         flash("Category Added", "success")
-        return redirect(url_for("profile", username = session['user']))
+        return redirect(url_for("profile", username=session['user']))
 
 
 @app.route("/delete_category/<category_id>")
 def delete_category(category_id):
     mongo.db.categories.remove({"_id": ObjectId(category_id)})
     flash("Category Successfully Deleted", "success")
-    return redirect(url_for("profile", username = session['user']))
+    return redirect(url_for("profile", username=session['user']))
 
 
-@app.route("/edit_user/<user_id>", methods = ["GET", "POST"])
+@app.route("/edit_user/<user_id>", methods=["GET", "POST"])
 def edit_user(user_id):
     if request.method == "POST":
         submit = {
@@ -252,27 +254,27 @@ def edit_user(user_id):
         }
         mongo.db.users.update({"_id": ObjectId(user_id)}, submit)
         flash("User Details Updated", "success")
-        return redirect(url_for("profile", username = session['user']))
+        return redirect(url_for("profile", username=session['user']))
 
     user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
-    return render_template("edit-user.html", user = user)
+    return render_template("edit-user.html", user=user)
 
 
 @app.route("/delete_user/<user_id>")
 def delete_user(user_id):
     mongo.db.users.remove({"_id": ObjectId(user_id)})
     flash("User Successfully Deleted", "success")
-    return redirect(url_for("profile", username = session['user']))
+    return redirect(url_for("profile", username=session['user']))
 
 
-@app.route("/add_favorite/<recipe_id>", methods = ["GET", "POST"])
+@app.route("/add_favorite/<recipe_id>", methods=["GET", "POST"])
 def add_favorite(recipe_id):
     user_id = mongo.db.users.find_one(
         {"username": session["user"]})["_id"]
     mongo.db.users.update({"_id": ObjectId(user_id)}, {"$push": {
         "my_favorites": recipe_id}})
     flash("Added to favorites", "success")
-    return redirect(url_for("recipe", recipe_id = recipe_id))
+    return redirect(url_for("recipe", recipe_id=recipe_id))
 
 
 @app.route("/delete_favorite/<recipe_id>")
@@ -282,9 +284,9 @@ def delete_favorite(recipe_id):
     mongo.db.users.update({"_id": ObjectId(user_id)}, {"$pull": {
         "my_favorites": recipe_id}})
     flash("Removed from favorites", "success")
-    return redirect(url_for("recipe", recipe_id = recipe_id))
+    return redirect(url_for("recipe", recipe_id=recipe_id))
 
 if __name__ == "__main__":
-    app.run(host = os.environ.get("IP"),
-            port = int(os.environ.get("PORT")),
-            debug = True)
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
